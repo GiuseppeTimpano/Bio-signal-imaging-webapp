900eea772f7554869ddefa68f7645f3376e9306f
GiuseppeTimpano <giuseppe.timpano@studenti.unicz.it>
2023-04-18 12:13:30 +0200

    form and validators for login and signup

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import render_template, url_for, flash, redirect
-from forms import LoginForm
+from forms import LoginForm, SignUpForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'f0d9750d6c694693beba7b033d08a66b'
@@ -15,16 +15,21 @@ def home():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data=='prova@unicz.it' and form.password=='prova':
-            flash('You have been logged in!')
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
             return redirect(url_for('home'))
         else:
             flash('Login Unsuccessful. Please check username and password', 'danger')
+            return redirect(url_for('home'))
     return render_template('login.html', title='Login', form=form)
 
 @app.route("/signup.html")
 def signup():
-    return render_template('signup.html', title='Sign_up')
+    form = SignUpForm()
+    if form.validate_on_submit():
+        if form.first_name.data == 'Giuseppe' and form.surname.data == 'Timpano':
+            flash('You have been signup!', 'success')
+    return render_template('signup.html', title='Sign_up', form=form)
 
 
 if __name__ == '__main__':
---
+++ flask_app/forms.py
@@ -0,0 +1,25 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', 
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remeber me')
+    submit = SubmitField('Login')
+
+class SignUpForm(FlaskForm):
+    first_name = StringField('Name', 
+                       validators=[DataRequired()])
+    surname = StringField('Surname', 
+                          validators=[DataRequired()])
+    email = StringField('Email', 
+                        validators=[DataRequired(), Email()])
+    confirm_email = StringField('Confirm Email', 
+                                validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', 
+                                     validators=[DataRequired()])
+    submit = SubmitField('Login')
