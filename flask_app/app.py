05c2afaf3033fa062fb52f62bd49eff88c892d95
GiuseppeTimpano <giuseppe.timpano@studenti.unicz.it>
2023-04-19 12:08:24 +0200

    add to nav operation for different users

--- flask_app/app.py
+++ flask_app/app.py
@@ -19,7 +19,7 @@ def login():
     if form.validate_on_submit():
         if form.email.data == 'admin@blog.com' and form.password.data == 'password':
             flash('You have been logged in!', 'success')
-            session['name'] = form.email.data
+            session['name'] = "admin"
             return redirect(url_for('home'))
         else:
             flash('Login Unsuccessful. Please check username and password', 'danger')
